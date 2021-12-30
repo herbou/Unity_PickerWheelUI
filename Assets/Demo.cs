dda5efd947dd29b5ca26a912ff2d79a15b6717f1
Cherarium <55898764+Cherarium@users.noreply.github.com>
2021-12-30 12:43:05 -0500

    Update Demo.cs
    Added cumulative (+/-) counter. Creating a more realistic experience.

--- Assets/Demo.cs
+++ Assets/Demo.cs
@@ -1,15 +1,13 @@
-﻿using UnityEngine ;
+using UnityEngine ;
 using EasyUI.PickerWheelUI ;
 using UnityEngine.UI ;
 
 public class Demo : MonoBehaviour {
    [SerializeField] private Button uiSpinButton ;
    [SerializeField] private Text uiSpinButtonText ;
-
    [SerializeField] private PickerWheel pickerWheel ;
-
-
    private void Start () {
+
       uiSpinButton.onClick.AddListener (() => {
 
          uiSpinButton.interactable = false ;
@@ -18,17 +16,15 @@ public class Demo : MonoBehaviour {
          pickerWheel.OnSpinEnd (wheelPiece => {
             Debug.Log (
                @" <b>Index:</b> " + wheelPiece.Index + "           <b>Label:</b> " + wheelPiece.Label
-               + "\n <b>Amount:</b> " + wheelPiece.Amount + "      <b>Chance:</b> " + wheelPiece.Chance + "%"
-            ) ;
+               + "\n <b>Point Rewarded</b> " + wheelPiece.value + "      <b>Chance:</b> " + wheelPiece.Chance + "%") ;
 
             uiSpinButton.interactable = true ;
             uiSpinButtonText.text = "Spin" ;
          }) ;
-
+         Debug.Log("Total Cumulative Score:" + pickerWheel.score);
          pickerWheel.Spin () ;
 
       }) ;
 
    }
-
 }
